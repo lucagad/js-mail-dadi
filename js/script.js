12f6a4d1f1115ab4ce2ffee3d84f42c98a890362
lucagad <lucagaddini@icloud.com>
2022-04-11 16:14:48 +0200

    Added: Mail Exercise

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 
-const classe61 = ["Domenico Agostino",
+const classe61 = [
+  "Domenico Agostino",
   "Mihai Badia",
   "Riccardo Becattini",
   "Manuel Benini",
@@ -12,7 +13,6 @@ const classe61 = ["Domenico Agostino",
   "Giancarlo Croce",
   "Enrico De Stefano",
   "Silvia Dugato",
-  "Carlo Duminuco Del Monte",
   "Francesco Ercoli",
   "Luca Gaddini",
   "Marco Gagliano",
@@ -39,3 +39,44 @@ const classe61 = ["Domenico Agostino",
 
 
 
+// 1. Creo un arrey con gli utenti autorizzati
+// 2. Chiedo d'inserire la mail
+// 3. Confonto la lista degli autorizzati con il valore inserito
+// 4. Stampo il risultato della verifica.
+
+const numItems = classe61.length;
+let userAuth = [];
+
+
+
+//Creo un elenco di mail autorizzate ed le inserirsco nel relativo array
+for (let i =0; i < numItems; i++){
+  const firstLastName = classe61[i].split(' ');
+  //const userMail = "mail"+i+"@mail.com";
+  const userMail = firstLastName[0].toLowerCase()+"."+firstLastName[1].toLowerCase()+"@boolean.careers";
+  userAuth.push (userMail);
+  console.log(userMail);
+  console.log(userAuth);
+};
+
+let mailInserted;
+const btnLogin = document.querySelector("#btn_login");
+
+btnLogin.addEventListener("click", function(){
+  const emailForm = document.querySelector("#InputEmail");
+  console.log(emailForm);
+
+  mailInserted = emailForm.value;
+  console.log("MAIL INSERITA:",mailInserted);
+
+  let flagAuth = false;
+
+  for(let i = 0; i< userAuth.length ; i++){
+    if (mailInserted === userAuth[i]) flagAuth = true;
+  }
+
+  if(flagAuth===true) {
+    alert("ACCESSO ESEGUITO")
+  } else alert("ACCESSO NEGATO. RIPROVA")
+
+});
